26ce076741635cbdf2a4f2728a7a1ebc20765f47
juanmafari <108095147+juanmafari@users.noreply.github.com>
2024-05-15 01:02:48 -0300

    botones independientes, seteo luces

--- juanmote/src/App.jsx
+++ juanmote/src/App.jsx
@@ -5,12 +5,14 @@ import { Model } from './Model';
 import { OrbitControls, CameraControls } from '@react-three/drei';
 
 
+
 function App() {
   const controlsRef = useRef();
   return (
     <Canvas style={{ width: '100%', height: '97vh'}} camera={{ position: [0,0,20]}} >
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+      <hemisphereLight color = {0xffffff} groundColor={0x000000} intensity={0.6} />
+      <pointLight position={[0,0,10]} distance={20} decay={2} />
+      <spotLight color={0xffffff} position={[0,0,5]} angle={Math.PI / 2} penumbra={0.1} distance={20} castShadow/>
       <OrbitControls ref={controlsRef} />
       <Model position={[0,0,0]} />
     </Canvas>
--- juanmote/src/Model.jsx
+++ juanmote/src/Model.jsx
@@ -8,12 +8,24 @@ import { useSpring, animated } from '@react-spring/three'
 export function Model(props) {
   const groupRef = useRef();
   const { nodes, materials } = useGLTF('/denadamakina.glb')
-  const [active, setActive] = useState(false)
-  const { position } = useSpring({ to: { position: active ? [0,0,-0.1] : [0,0,0] } })
-  
+  const [active1, setActive1] = useState(false);
+  const [active2, setActive2] = useState(false);
+  const [active3, setActive3] = useState(false);
+  const [active4, setActive4] = useState(false);
+  const [active5, setActive5] = useState(false);
+  const [active6, setActive6] = useState(false);
+
+  const spring1 = useSpring({ to: { z: active1 ? -0.1 : 0 } });
+  const spring2 = useSpring({ to: { z: active2 ? -0.1 : 0 } });
+  const spring3 = useSpring({ to: { z: active3 ? -0.1 : 0 } });
+  const spring4 = useSpring({ to: { z: active4 ? -0.1 : 0 } });
+  const spring5 = useSpring({ to: { z: active5 ? -0.1 : 0 } });
+  const spring6 = useSpring({ to: { z: active6 ? -0.1 : 0 } });
+
+  /*
   useFrame(({ clock }) => {
     groupRef.current.rotation.y = 0.5*Math.sin(0.1*clock.getElapsedTime())
-  })
+  })*/
 
   return (
     <group {...props} dispose={null} ref={groupRef}>
@@ -119,64 +131,61 @@ export function Model(props) {
         castShadow
         receiveShadow
         geometry={nodes.SwitchKeycap_v6.geometry}
-        material={materials['Acero_-_Satinado.002']}
+        material={materials['HP_3D_HR_CB_PA_12_(con_impresora_3D_en_color_HP_Jet_Fusion_.001']}
         rotation={[Math.PI / 2, 0, 0]}
-        position={position} 
-        onPointerDown={() => setActive(true)}
-        onPointerUp={() => setActive(false)}
+        position-z={spring1.z} 
+        onPointerDown={() => setActive1(true)}
+        onPointerUp={() => setActive1(false)}
       />
       <animated.mesh
         castShadow
         receiveShadow
         geometry={nodes['SwitchKeycap_v6_(1)'].geometry}
-        material={materials['Acero_-_Satinado.002']}
+        material={materials['HP_3D_HR_CB_PA_12_(con_impresora_3D_en_color_HP_Jet_Fusion_.001']}
         rotation={[Math.PI / 2, 0, 0]}
-        position={position} 
-        onPointerDown={() => setActive(true)}
-        onPointerUp={() => setActive(false)}
+        position-z={spring2.z}  
+        onPointerDown={() => setActive2(true)}
+        onPointerUp={() => setActive2(false)}
       />
       <animated.mesh
         castShadow
         receiveShadow
         geometry={nodes['SwitchKeycap_v6_(1)_(1)'].geometry}
-        material={materials['Acero_-_Satinado.002']}
+        material={materials['HP_3D_HR_CB_PA_12_(con_impresora_3D_en_color_HP_Jet_Fusion_.001']}
         rotation={[Math.PI / 2, 0, 0]}
-        position={position} 
-        onPointerDown={() => setActive(true)}
-        onPointerUp={() => setActive(false)}
+        position-z={spring3.z}  
+        onPointerDown={() => setActive3(true)}
+        onPointerUp={() => setActive3(false)}
       />
       <animated.mesh
         castShadow
         receiveShadow
         geometry={nodes['SwitchKeycap_v6_(1)_(1)_(1)'].geometry}
-        material={materials['Acero_-_Satinado.002']}
+        material={materials['HP_3D_HR_CB_PA_12_(con_impresora_3D_en_color_HP_Jet_Fusion_.001']}
         rotation={[Math.PI / 2, 0, 0]}
-        position={position} 
-        onPointerDown={() => setActive(true)}
-        onPointerUp={() => setActive(false)}
+        position-z={spring4.z}  
+        onPointerDown={() => setActive4(true)}
+        onPointerUp={() => setActive4(false)}
       />
       <animated.mesh
         castShadow
         receiveShadow
         geometry={nodes['SwitchKeycap_v6_(1)_(2)'].geometry}
-        material={materials['Acero_-_Satinado.002']}
+        material={materials['HP_3D_HR_CB_PA_12_(con_impresora_3D_en_color_HP_Jet_Fusion_.001']}
         rotation={[Math.PI / 2, 0, 0]}
-        position={position} 
-        onPointerDown={() => setActive(true)}
-        onPointerUp={() => setActive(false)}
+        position-z={spring5.z}  
+        onPointerDown={() => setActive5(true)}
+        onPointerUp={() => setActive5(false)}
       />
       <animated.mesh
         castShadow
         receiveShadow
         geometry={nodes['SwitchKeycap_v6_(2)'].geometry}
-        material={materials['Acero_-_Satinado.002']}
+        material={materials['HP_3D_HR_CB_PA_12_(con_impresora_3D_en_color_HP_Jet_Fusion_.001']}
         rotation={[Math.PI / 2, 0, 0]}
-        position={position} 
-        onPointerDown={() => setActive(true)}
-        onPointerUp={() => setActive(false)}
-        position={position} 
-        onPointerDown={() => setActive(true)}
-        onPointerUp={() => setActive(false)}
+        position-z={spring6.z}  
+        onPointerDown={() => setActive6(true)}
+        onPointerUp={() => setActive6(false)}
         
       />
       <mesh
