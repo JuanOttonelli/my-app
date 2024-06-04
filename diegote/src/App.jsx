4a51ecc2af074253e53741757e7b3dc613dddead
diagodiogo <diegoduranlanzaddl@gmail.com>
2024-06-04 10:24:59 -0300

    a

--- diegote/src/App.jsx
+++ diegote/src/App.jsx
@@ -10,11 +10,12 @@ function App() {
   const controlsRef = useRef();
   return (
     <Canvas style={{ width: '100%', height: '97vh'}} camera={{ position: [0,0,40]}} >
-      <hemisphereLight color = {0xffffff} groundColor={0x000000} intensity={0.6} />
+      <hemisphereLight color = {0xffffff} groundColor={0x000000} intensity={1} />
       <pointLight position={[0,0,10]} distance={20} decay={2} />
       <spotLight color={0xffffff} position={[0,0,5]} angle={Math.PI / 2} penumbra={0.1} distance={20} castShadow/>
+      <spotLight color={0xffffff} position={[0,0,0]} angle={Math.PI / 2} penumbra={0.1} distance={20} castShadow/>
       <OrbitControls ref={controlsRef} />
-      <Model position={[-10,-10,0]} />
+      <Model position={[-10,0,0]} />
     </Canvas>
   );
 }
